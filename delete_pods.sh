3e080e384c89ea7de74593a8495563d655227343
root <root@onekube-ip-192-168-122-27.localdomain>
2020-09-18 00:30:16 +0000

    revision_5. Merged changes from https://github.com/sa3036/rts_pivotal_bank_/compare/5f661bd..9e95baf

---
+++ delete_pods.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#
+kubectl delete -f app.yml
--- kill_now_terminating_pods.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# kill all terminating pods forcefully
-for p in $(kubectl get pods | grep Terminating | awk '{print $1}'); do kubectl delete pod $p --grace-period=0 --force;done
